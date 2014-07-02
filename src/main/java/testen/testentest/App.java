fbce49b243dd3ccff94e52e7d95abdb06c64624f
ISP Guest 72 <guest72@pcclt13.isp.uni-luebeck.de>
2014-07-02 16:53:23 +0200

    initial

---
+++ src/main/java/testen/testentest/App.java
@@ -0,0 +1,13 @@
+package testen.testentest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
